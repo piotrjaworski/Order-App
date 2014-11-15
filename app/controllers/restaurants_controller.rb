5e50931698710355407100d7ce40a0cfb6c0e62a
Piotr Jaworski <piotr.jaworski@live.com>
2014-11-15 20:48:39 +0100

    Small changes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,6 +12,7 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
+    @restaurants = Restaurant.all
     if @restaurant.save
       @restaurant.update_attributes(user_id: current_user.id)
       render :hide_form
@@ -42,6 +43,7 @@ class RestaurantsController < ApplicationController
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
+    @restaurants = Restaurant.all
   end
 
   private
