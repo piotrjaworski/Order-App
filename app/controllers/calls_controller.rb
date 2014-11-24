0d1372498e8c57bef00d6351ed4b11a5fa2064fe
Piotr Jaworski <piotr.jaworski@live.com>
2014-11-24 01:43:12 +0100

    Changes in controllers

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,5 +1,4 @@
-class CallsController < ApplicationController
-  helper_method :show_products, :count_price, :count_total_price
+class CallsController < MethodsController
 
   def new
     @call = Call.new
@@ -29,32 +28,6 @@ class CallsController < ApplicationController
 
   def call_params
     params.require(:call).permit(:user_id, :delivery_time)
-  end
-
-  def show_products(object)
-    object.products.map(&:name).join(", ")
-  end
-
-  def today_orders
-    @orders = Order.where("updated_at >= ?", Time.zone.now.beginning_of_day)
-  end
-
-  def count_price(object)
-    sum = []
-    object.products.each do |product|
-      sum << product.price
-    end
-    @sum = sum.inject(:+)
-  end
-
-  def count_total_price(objects)
-    today_sum = []
-    objects.each do |order|
-      order.products.each do |product|
-        today_sum << product.price
-      end
-    end
-    @today_sum = today_sum.inject(:+)
   end
 
 end
---
+++ app/controllers/methods_controller.rb
@@ -0,0 +1,32 @@
+class MethodsController < ApplicationController
+  helper_method :show_products, :count_price, :count_total_price
+  before_action :authenticate_user!, except: :index
+
+  private
+
+  def count_price(object)
+    sum = []
+    object.products.each do |product|
+      sum << product.price
+    end
+    @sum = sum.inject(:+)
+  end
+
+  def count_total_price(objects)
+    today_sum = []
+    objects.each do |order|
+      order.products.each do |product|
+        today_sum << product.price
+      end
+    end
+    @today_sum = today_sum.inject(:+)
+  end
+
+  def show_products(object)
+    object.products.map(&:name).join(", ")
+  end
+
+  def today_orders
+    @orders = Order.where("updated_at >= ?", Time.zone.now.beginning_of_day)
+  end
+end
