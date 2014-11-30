b647c9d6918f85e3cb43aa362412c88d75b3a61f
Piotr Jaworski <piotr.jaworski@live.com>
2014-11-30 18:06:00 +0100

    Deleted test folder

--- test/controllers/collects_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CollectsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/collects_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CollectsHelperTest < ActionView::TestCase
-end
